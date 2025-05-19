fbd5f34cd4ad1d729532849005ee9c1738d0b8d3
Mariana Ribeiro <mrs.ribeirosm@gmail.com>
2025-05-19 19:15:01 -0300

    Criação da seção de Especialidade e criação do menu responsivo

--- Javascript/index.js
+++ Javascript/index.js
@@ -1,3 +1,21 @@
+let itens = document.querySelector("menu");
+
+function mudouTamanho() {
+                    if (window.innerWidth >=1000 ) {
+                        itens.style.display = 'block'
+                } else {
+                    itens.style.display = 'none'
+                }
+            }
+        
+        function clickMenu() {
+            if (itens.style.display == 'block') {
+                itens.style.display = 'none'
+            } else {
+                itens.style.display = 'block'
+            }
+        }
+
 var especialidadeCaixas = document.querySelectorAll('.especialidade-caixa');
 
 especialidadeCaixas.forEach(caixa => {
